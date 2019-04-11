634bfb66e23dd8ef9fbce849c866fa5db5f58412
linye <a1851yl@aiit.ac.jp>
2019-04-11 22:03:42 +0900

    feelings prototype

--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -1,7 +1,39 @@
 class FeelingsController < ApplicationController
+
+  $title = ""
+  $yes_count = 0
+  $no_count = 0
+
   def new
   end
 
+  def create
+    selection = params[:title]
+    if selection && selection.length > 0
+      $title = selection
+    end
+    redirect_to feelings_index_path
+  end
+
   def index
+    if session[:voted]
+      redirect_to feelings_result_path
+    end
+  end
+
+  def result
+  end
+
+  def vote
+    selection = params[:selection]
+    if selection == "true"
+      $yes_count += 1
+    elsif
+      $no_count += 1
+    end
+    puts $yes_count
+    puts $no_count
+    session[:voted] = true
+    redirect_to feelings_result_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'feelings/new'
   get 'feelings/index'
+  post 'feelings/vote', to: "feelings#vote"
+  get 'feelings/result', to: "feelings#result"
+  post 'feelings/create', to: "feelings#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
