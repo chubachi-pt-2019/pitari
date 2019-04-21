d29971f04aaae5fd290d8a7fbe52d602dc8407e4
JIN JONGWOO <a1824jj@aiit.ac.jp>
2019-04-21 15:21:03 +0900

    ハードコーディングごり押し！

--- app/controllers/discuss_controller.rb
+++ app/controllers/discuss_controller.rb
@@ -10,14 +10,30 @@ class DiscussController < ApplicationController
   end
 
   def create
-    Discuss.new(discuss_params).save
+
+    discuss = Discuss.new
+    discuss.name = discuss_params["name"]
+    discuss.save
+
+    user = User.new
+    user.name = discuss_params["user"]["name"]
+    user.discuss_id = discuss.id
+    user.save
+
+    agenda = Agenda.new
+    agenda.name = discuss_params["user"]["agenda"]["name"]
+    agenda.opinion = discuss_params["user"]["agenda"]["opinion"]
+    agenda.user_id = user.id
+    agenda.save
+
     render "show"
+
   end
   
   private
   
     def discuss_params
-      params.require(:discuss).permit(:name)
+      params.require(:discuss).permit(:name, {user: [:name, {agenda: [:name, :opinion]} ] } )
     end
 
     
