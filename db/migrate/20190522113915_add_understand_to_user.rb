8aa36cafbe706c65274eec24e5a262245fa43e9c
Dozono <dozono@dozono.net>
2019-05-25 13:14:14 +0900

    Modify migration for understand default value

--- db/migrate/20190522113915_add_understand_to_user.rb
+++ db/migrate/20190522113915_add_understand_to_user.rb
@@ -1,5 +1,5 @@
 class AddUnderstandToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :understand, :integer
+    add_column :users, :understand, :integer, default: 4
   end
 end
