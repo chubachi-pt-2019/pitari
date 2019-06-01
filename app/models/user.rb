1415693afdd5b5101b985690ad2558b231b0af84
liuxudong19 <a1750xl@aiit.ac.jp>
2019-06-01 13:42:27 +0900

    #136

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   after_create_commit { JoinMeetingJob.perform_later self }
-  after_update_commit { UpdateUnderstandJob.perform_later self }
   validates :name, presence: true
   belongs_to :discuss
   has_many :agendas
